4c5ebf1e95679df3a80169f5dfe13dd9efaadd67
vedang singh <vedang012@gmail.com>
2024-08-28 20:35:42 +0530

    first commit

---
+++ Stores/AppStore.cs
@@ -0,0 +1,41 @@
+﻿namespace BlazorNavUI.Stores;
+
+public class AppStore
+{
+    public Action? OnChange;
+
+    private bool isNavOpen = false;
+
+    public bool IsNavOpen
+    {
+        get { return isNavOpen; }
+        set
+        {
+            isNavOpen = value;
+            OnChange?.Invoke();
+        }
+    }
+
+    public string BottomNavMenuBtnText = "Checkout";
+    public string BottomNavMenuBtnActionUrl = "/checkout";
+    private bool bottomNavMenuBtnVisible = false;
+    public bool BottomNavMenuBtnVisible
+    {
+        get { return bottomNavMenuBtnVisible; }
+        set 
+        { 
+            bottomNavMenuBtnVisible = value;
+            OnChange?.Invoke();
+        }
+    }
+    
+
+    public void ShowBottomNavMenuBtn(string txt, string actionUrl)
+    {
+        BottomNavMenuBtnVisible = true;
+        BottomNavMenuBtnActionUrl = actionUrl;
+        BottomNavMenuBtnText = txt;
+
+        OnChange?.Invoke();
+    }
+}
